2b40ec642e3176273dabb8b0a61163f5bafbce07
Joshua Lee <mrlee@Mac.fios-router.home>
2025-04-04 16:30:28 -0400

    Modified auth file to add mew variables

--- gomotive/auth.py
+++ gomotive/auth.py
@@ -4,20 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_gomotive_access_token():
-    auth_url = os.getenv("GOMOTIVE_AUTH_URL")
+def get_gomotive_access_token(auth_code: str):
     client_id = os.getenv("GOMOTIVE_CLIENT_ID")
     client_secret = os.getenv("GOMOTIVE_CLIENT_SECRET")
+    token_url = os.getenv("GOMOTIVE_TOKEN_URL")
+    redirect_uri = os.getenv("GOMOTIVE_REDIRECT_URI")
 
-    payload = {
-        "grant_type": "client_credentials",
+    data = {
+        "grant_type": "authorization_code",
+        "code": auth_code,
+        "redirect_uri": redirect_uri,
         "client_id": client_id,
-        "client_secret": client_secret,
-        "scope": "all"
+        "client_secret": client_secret
     }
 
-    response = requests.post(auth_url, data=payload)
+    response = requests.post(token_url, data=data)
     if response.status_code == 200:
-        return response.json()["access_token"]
+        return response.json()
     else:
         raise Exception(f"Failed to get token: {response.text}")
